fe1e5d62b920a8774e93860913580d4da917aca7
Aditya Dedhia <adityadedhia@hey.com>
2026-03-07 07:16:44 +0000

    Added dynamic env type.

--- manage.py
+++ manage.py
@@ -4,10 +4,15 @@
 import os
 import sys
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tidalsight.settings.local")
+    env_type = os.environ.get("ENV_TYPE", "local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"tidalsight.settings.{env_type}")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- tidalsight/wsgi.py
+++ tidalsight/wsgi.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 
 import os
 
+from dotenv import load_dotenv
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tidalsight.settings.prod")
+load_dotenv()
+
+env_type = os.environ.get("ENV_TYPE", "local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"tidalsight.settings.{env_type}")
 
 application = get_wsgi_application()
